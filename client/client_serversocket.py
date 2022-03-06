c3d12502db843839d98e380e87e250c8e722efbb
akmalzakia <zakiasmara11@gmail.com>
2022-03-06 17:52:01 +0700

    add serversocket.py

---
+++ client/client_serversocket.py
@@ -0,0 +1,78 @@
+import math
+import socket
+import sys
+
+sys.path.append('../')
+import header_utils
+
+
+HEADER = 256
+BUFFER_SIZE = 1024
+PORT = 5050
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+SERVER = "192.168.1.15"
+ADDR = (SERVER, PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+def get_header():
+    header = client.recv(HEADER).decode(FORMAT)
+    return header
+
+def recv(header):
+    msg_length = header_utils.read_msg_header(header)
+    if msg_length:
+        msg_length = int(msg_length)
+        msg = client.recv(msg_length).decode(FORMAT)
+        print(msg)
+
+        return 1
+    
+    return 0
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    
+    header = header_utils.build_msg_header(message)
+    client.send(header)
+    client.send(message)
+
+def recv_file(header):
+    file_name, file_size = header_utils.read_file_header(header)
+    max_loop = math.ceil(file_size / BUFFER_SIZE)
+    loop_counter = 0
+    with open(f'{file_name}', 'wb') as f:
+        while True:
+            print('receiving data ...')
+            data = client.recv(BUFFER_SIZE)
+            loop_counter += 1
+            f.write(data)
+            if loop_counter == max_loop:
+                break
+            
+    f.close()
+    print('download success')
+
+def start():
+    connected = True
+    while connected:
+
+        # Get message from server
+        header = get_header()
+        msg_type = header_utils.read_header_type(header)
+        if msg_type == 'file':
+            recv_file(header)
+        elif msg_type == 'msg':
+            recv(header)
+
+        print(">>", end=' ', flush=True)
+        msg = input()
+        send(msg)
+
+
+
+
+
+start()
---
+++ server/server_serversocket.py
@@ -0,0 +1,109 @@
+from email import header
+import os
+import socket
+import sys
+import threading
+from os import walk
+
+sys.path.append('./')
+import header_utils
+
+# Constants
+HEADER = 256
+BUFFER_SIZE = 1024
+PORT = 5050
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+DATASET = './server/dataset'
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+files = []
+
+def listFiles(conn):
+    files = next(walk(DATASET), (None, None, []))[2]
+    message = "List of files :\n"
+    for idx, f in enumerate(files):
+        message += f"{idx} : {f}\n"
+
+    return message
+
+def send(conn, addr, msg):
+    
+    message = msg.encode(FORMAT)
+    
+    header = header_utils.build_msg_header(message)
+    print(f'[SENDING] Server is sending {len(msg)} bytes of data to {addr}')
+    conn.send(header)
+    conn.send(message)
+
+def recv(conn, addr):
+    header = conn.recv(HEADER).decode(FORMAT)
+    msg_length = header_utils.read_msg_header(header)
+    if msg_length:
+        msg_length = int(msg_length)
+        msg = conn.recv(msg_length).decode(FORMAT)
+        print(f"[{addr}] {msg}")
+
+        if(msg != DISCONNECT_MESSAGE):
+            return msg
+            
+    return 0
+
+def send_file(conn, addr, file):
+    file_path = f'{DATASET}/{file}'
+    if os.path.isfile(file_path):
+        file_size = os.path.getsize(file_path)
+        header = header_utils.build_file_header(file, file_size)
+        print(f'[SENDING] Server is sending file {file} to {addr}')
+        conn.send(header)
+        f = open(file_path, 'rb')
+        l = f.read(1024)
+        while l:
+            conn.send(l)
+            l = f.read(BUFFER_SIZE)
+        print(f'[SUCCESS] Server successfully send file {file} to {addr}')
+        f.close()
+    else:
+        print(f'[ERROR] Server failed to send file {file} to {addr}')
+
+    
+
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected.")
+    send(conn, addr, f"{listFiles(conn)}Select file you want to download with command 'unduh nama_file'")
+    connected = True
+    while connected:
+        res = recv(conn, addr)
+        if not res:
+            connected = False
+        else:
+            cmd = res.split(' ')
+            if cmd[0] == 'unduh':
+                dl_file = cmd[1]
+                send_file(conn, addr, dl_file)
+            else:
+                send(conn, addr, "Invalid command, please try again")
+
+            
+    conn.close()
+
+def start():
+    server.listen()
+    print(f"[LISTENING] Server is listening on {SERVER}")
+    while True:
+        conn, addr = server.accept()
+        handle_client(conn,addr)
+        # thread = threading.Thread(target=handle_client, args=(conn, addr))
+        # thread.start()
+        # print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+        
+
+
+
+
+print("[STARTING] server is starting ...")
+start()
